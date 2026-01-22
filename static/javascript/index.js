fb155e7a91b47774c0b4bcd3b911fd1b19ae91fe
Noah <bigspaceships56@gmail.com>
2026-01-22 13:03:41 -0500

    User info on client

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -23,8 +23,37 @@ function enterQueue(type) {
   })
 }
 
+function parseJwt(token) {
+  if (!token) {
+    return;
+  }
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+
+  const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
+
+async function getUserInfo() {
+  const cookie = await cookieStore.get("Auth");
+
+  return parseJwt(cookie.value).UserInfo;
+}
+
+function setUserInfo(userInfo) {
+  const username = userInfo.preferred_username;
+  const name = userInfo.name;
+  document.getElementById("profile-pic").src = `https://profiles.csh.rit.edu/image/${username}`;
+  document.getElementById("profile-name").innerText = name;
+}
+
 async function main() {
   console.log(await getQueue())
+  userInfo = await getUserInfo()
+  setUserInfo(userInfo)
 }
 
 main()
